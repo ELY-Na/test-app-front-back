0be6c3aeb4ca1fb7ab9b7af1406a2c1dcf58e1dc
ELY-Na <najia.elyouzghi@gmail.com>
2026-06-27 22:51:53 +0200

    test: add units test for SpringBootSecurityJwtApplication and update pom file

--- back/src/test/java/com/openclassrooms/starterjwt/SpringBootSecurityJwtApplicationTests.java
+++ back/src/test/java/com/openclassrooms/starterjwt/SpringBootSecurityJwtApplicationTests.java
@@ -1,6 +1,11 @@
 package com.openclassrooms.starterjwt;
 
+import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.Mockito.*;
+
 import org.junit.jupiter.api.Test;
+import org.mockito.MockedStatic;
+import org.springframework.boot.SpringApplication;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
@@ -10,4 +15,16 @@ public class SpringBootSecurityJwtApplicationTests {
 	public void contextLoads() {
 	}
 
+	@Test
+	void shouldCallMain() {
+
+		try (MockedStatic<SpringApplication> mocked = mockStatic(SpringApplication.class)) {
+
+			SpringBootSecurityJwtApplication.main(new String[] {});
+
+			mocked.verify(() -> SpringApplication.run(
+					SpringBootSecurityJwtApplication.class,
+					new String[] {}));
+		}
+	}
 }
